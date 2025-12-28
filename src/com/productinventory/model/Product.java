42120d8a8e41b96d5921f4c0cf5414c67dee840d
ImRehan-28 <173938869+ImRehan-28@users.noreply.github.com>
2025-12-28 09:02:30 +0530

    additional to Product class

--- src/com/productinventory/model/Product.java
+++ src/com/productinventory/model/Product.java
@@ -1,4 +1,44 @@
 package com.productinventory.model;
-public class Product{
+public class Product
+{
+	private int productId;
+	private String productName;
+	private int quantity;
+	private double price;
 	
+	public void setProductId(int productId)
+	{
+		this.productId=productId;
+	}
+	
+	public void setProductName()
+	{
+		this.productName=productName;
+	}
+	public void setQuantity()
+	{
+		this.quantity=quantity;
+	}
+	public void setPrice()
+	{
+		this.price=price;
+	}
+	
+	public int getProductId()
+	{
+		return productId;
+	}
+	
+	public String getProductName()
+	{
+		return productName;
+	}
+	public int getQuantity()
+	{
+		return quantity;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
 }
---
+++ src/com/productinventory/model/Supplier.java
@@ -0,0 +1,11 @@
+package com.productinventory.model;
+import java.time.LocalDateTime; 
+public class Supplier {
+	private int SupplierId;
+	private String name;
+	private String emailAddress;
+	private Long phno;
+	private String status;
+	private LocalDateTime createdAt;
+	
+}
